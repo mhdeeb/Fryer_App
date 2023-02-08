2b4347e6957b96c2dea8f5d75c47d56d46d183b8
Mohamed Hussien El-Deeb <s-mohamed.eldeeb@zewailcity.edu.eg>
2023-02-08 09:04:37 +0200

    Remove output

--- output/core/wiring_pulse.S.d
+++
@@ -1,2 +0,0 @@
-c:\Users\eldeeb\Desktop\Fryer_APP\output\core\wiring_pulse.S.o: \
- C:\Users\eldeeb\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino\wiring_pulse.S
--- output/libraries/Wire/Wire.cpp.d
+++
@@ -1,8 +0,0 @@
-c:\Users\eldeeb\Desktop\Fryer_APP\output\libraries\Wire\Wire.cpp.o: \
- C:\Users\eldeeb\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\libraries\Wire\src\Wire.cpp \
- C:\Users\eldeeb\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\libraries\Wire\src\utility/twi.h \
- C:\Users\eldeeb\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\libraries\Wire\src\Wire.h \
- C:\Users\eldeeb\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino/Stream.h \
- C:\Users\eldeeb\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino/Print.h \
- C:\Users\eldeeb\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino/WString.h \
- C:\Users\eldeeb\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino/Printable.h
